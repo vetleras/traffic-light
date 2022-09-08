4451af299ee13a4ff7c3b5fe7f8fd41c08bfd882
vetleras <vetleras@gmail.com>
2022-09-08 12:39:10 +0200

    Task 2: implement exit event

--- src/exercises/PeriodicTimerMachine.java
+++ src/exercises/PeriodicTimerMachine.java
@@ -6,22 +6,29 @@ import runtime.Scheduler;
 import runtime.Timer;
 
 public class PeriodicTimerMachine implements IStateMachine {
-	
-	private static final String START = "Start", STOP = "Stop", TIMER_1 = "t1";	
-	public static final String[] EVENTS = {START, STOP};
-	private enum STATES {IDLE, ACTIVE, FINAL}
+
+	private static final String START = "Start", STOP = "Stop", TIMER_1 = "t1", EXIT = "Exit";
+	public static final String[] EVENTS = { START, STOP, EXIT };
+
+	private enum STATES {
+		IDLE, ACTIVE, FINAL
+	}
+
 	private Timer t1 = new Timer("t1");
 	protected STATES state = STATES.IDLE;
 
 	public int fire(String event, Scheduler scheduler) {
-		if(state==STATES.IDLE) {
-			if(event.equals(START)) {
+		if (state == STATES.IDLE) {
+			if (event.equals(START)) {
 				t1.start(scheduler, 1000);
 				state = STATES.ACTIVE;
 				return EXECUTE_TRANSITION;
-			}  
-		} else if(state==STATES.ACTIVE) {
-			if(event.equals(STOP)) {
+			} else if (event.equals(EXIT)) {
+				state = STATES.FINAL;
+				return TERMINATE_SYSTEM;
+			}
+		} else if (state == STATES.ACTIVE) {
+			if (event.equals(STOP)) {
 				t1.stop();
 				state = STATES.IDLE;
 				return EXECUTE_TRANSITION;
@@ -30,15 +37,19 @@ public class PeriodicTimerMachine implements IStateMachine {
 				t1.start(scheduler, 1000);
 				state = STATES.ACTIVE;
 				return EXECUTE_TRANSITION;
+			} else if (event.equals(EXIT)) {
+				t1.stop();
+				state = STATES.FINAL;
+				return TERMINATE_SYSTEM;
 			}
 		}
 		return DISCARD_EVENT;
 	}
-	
+
 	public static void main(String[] args) {
 		IStateMachine stm = new PeriodicTimerMachine();
 		Scheduler s = new Scheduler(stm);
-		
+
 		EventWindow w = new EventWindow(EVENTS, s);
 		w.show();
 
